ca794286d66b43f54da98ed042a59b64ce8ce92e
AleaxndriucIonut <ionut.alexandriuc@student.usv.ro>
2024-10-29 15:52:35 +0200

    Add project files.

---
+++ nush/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nush
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Vertex 3 RGB: 0.0, 0.0, 1.0");
+            Console.WriteLine($"Vertex 7 RGB: 5.0, 4.0, 3.0");
+            Console.WriteLine($"Vertex 7 RGB: 5.0, 4.0, 3.0");
+            Console.WriteLine($"Vertex 7 RGB: 5.0, 4.0, 3.0");
+            Console.WriteLine($"Vertex 7 RGB: 5.0, 4.0, 3.0");
+        }
+    }
+}
